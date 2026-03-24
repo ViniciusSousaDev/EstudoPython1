f05142840152bdd10dc96c1df3f21a0c7ff943fd
Vinicius Soares de Sousa <120108578+ViniciusSousaDev@users.noreply.github.com>
2026-03-23 21:21:40 -0300

    exercicios aula 23.03

---
+++ exercicio16.py
@@ -0,0 +1,4 @@
+## Contagem regressiva
+
+for numero in range (10, 0, -1):
+    print(numero)
---
+++ exercicio17.py
@@ -0,0 +1,7 @@
+## Soma de 1 até 100
+
+numero = 0
+soma = numero + 1
+
+for soma in range(1,101):
+    print(f'{soma} = {soma+soma}')
